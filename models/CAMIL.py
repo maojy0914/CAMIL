5481d32a59b0146d9a96f1129bb12ad493acb81a
maojy <maojinyang666@gmail.com>
2024-11-22 14:24:26 +0800

    second commit

--- models/CAMIL.py
+++ models/CAMIL.py
@@ -29,91 +29,175 @@ class TransLayer(nn.Module):
         return x
 
 class MCAB(nn.Module):
-    def __init__(self,dim=512):
-        super(MCAB, self).__init__()
-        self.dconv1 = nn.Conv2d(dim,dim,kernel_size=3,stride=1, padding=3 // 2, groups=dim)
-        self.pconv1 = nn.Conv2d(dim, dim, kernel_size=1, stride=1, groups=1)
-        self.dconv2 = nn.Conv2d(dim,dim,kernel_size=5,stride=1, padding=5 // 2, groups=dim)
-        self.pconv2 = nn.Conv2d(dim, dim, kernel_size=1, stride=1, groups=1)
-        self.eca_layer = ECA_Layer(dim)
+    def __init__(self,dim=512,temperature=3):
+        super().__init__()
+
+        self.local_feature3 = nn.Sequential(
+            nn.Conv2d(dim,dim,kernel_size=3,stride=1, padding=3 // 2, groups=dim),
+            nn.ReLU(),
+            nn.Conv2d(dim, dim, kernel_size=1, stride=1, groups=1)
+        )
+
+        self.local_feature5 = nn.Sequential(
+            nn.Conv2d(dim, dim, kernel_size=5, stride=1, padding=5 // 2, groups=dim),
+            nn.ReLU(),
+            nn.Conv2d(dim, dim, kernel_size=1, stride=1, groups=1)
+        )
+
+
+        ## self.eca_layer = ECA_Layer(dim)
+        # self.cam = CAM_1(n_channel=dim,mlp_r=2,temperature=temperature)
+        self.cam = CAM(n_channel=dim, mlp_r=2, temperature=temperature)
 
     def forward(self,x):
         B, N, C = x.shape
-        cls_token, feat_token = x[:, 0], x[:, 1:]
 
-        _H, _W = int(np.ceil(np.sqrt(N-1))), int(np.ceil(np.sqrt(N-1)))
-        add_length = _H * _W - (N - 1)
-        zero_tensor = torch.zeros(B, add_length, C).cuda()
-        x_pad = torch.cat([feat_token, zero_tensor], dim=1) # [B,_H*_W,C]
+        _H, _W = int(np.ceil(np.sqrt(N))), int(np.ceil(np.sqrt(N)))
+        add_length = _H * _W - N
+        zero_tensor = torch.zeros(B, add_length, C,device=x.device)
+        
+        x_pad = torch.cat([x, zero_tensor], dim=1) # [B,_H*_W,C]
+
         cnn_feat = x_pad.transpose(1, 2).view(B, C, _H, _W)
-        x3_3 = self.pconv1(self.dconv1(cnn_feat))
-        x5_5 = self.pconv2(self.dconv2(cnn_feat))
+        x3_3 = self.local_feature3(cnn_feat)
+        x5_5 = self.local_feature5(cnn_feat)
         x = cnn_feat + x3_3 + x5_5
-        y = self.eca_layer(x)
-        x = y * x3_3 + y * x5_5
+
+        y1,y2,y3 = self.cam(x) # [B,C,1,1]
+        x = y1 * cnn_feat + y2 * x3_3 + y3 * x5_5
         x = x.flatten(2).transpose(1, 2)    # [B,_H*_W,C]
-        x = x[:, :N-1]
-        x = torch.cat((cls_token.unsqueeze(1), x), dim=1)
+        x = x[:, :N]
         return x
 
-class ECA_Layer(nn.Module):
-    """Constructs a ECA module.
-
-    Args:
-        channel: Number of channels of the input feature map
-        k_size: Adaptive selection of kernel size
-    """
-    def __init__(self, channel, k_size=3):
-        super(ECA_Layer, self).__init__()
+class CAM(nn.Module):
+    def __init__(self,n_channel,mlp_r=2,temperature=1):
+        super().__init__()
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
-        self.sigmoid = nn.Sigmoid()
+        self.linear = nn.Sequential(
+            nn.Linear(n_channel, n_channel // mlp_r),
+            nn.ReLU(),
+            nn.Linear(n_channel // mlp_r, 3 * n_channel)
+        )
+        self.temperature = temperature
+
+    def forward(self,x):
+        B,C,_,_ = x.shape
+        max = self.max_pool(x)
+        avg = self.avg_pool(x)
+        max = self.linear(max.view(B,C)).view(B,3*C,1,1)
+        avg = self.linear(avg.view(B,C)).view(B,3*C,1,1)
+        y = torch.sigmoid((max + avg) * self.temperature)
+        y_1 = y[:,:C]
+        y_2 = y[:,C:2*C]
+        y_3 = y[:,2*C:]
+        return y_1,y_2,y_3
+
+class Block(nn.Module):
+    def __init__(self,dim=512,temperature=3):
+        super().__init__()
+        self.trans_layer = TransLayer(dim=dim)
+        self.mcab = MCAB(dim=dim,temperature=temperature)
+
+    def forward(self,x):
+        x = self.trans_layer(x)
+        x = self.mcab(x)
+
+        return x
+
+def initialize_weights(module):
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight)
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+        elif isinstance(m, nn.BatchNorm1d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+
+
+class Attn_Net(nn.Module):
+
+    def __init__(self, L=512, D=256, dropout=False, n_classes=1):
+        super(Attn_Net, self).__init__()
+        self.module = [
+            nn.Linear(L, D),
+            nn.Tanh()]
+
+        if dropout:
+            self.module.append(nn.Dropout(0.25))
+
+        self.module.append(nn.Linear(D, n_classes))
+
+        self.module = nn.Sequential(*self.module)
 
     def forward(self, x):
+        return self.module(x), x  # N x n_classes
 
-        y = self.avg_pool(x)
+class Attn_Net_Gated(nn.Module):
+    def __init__(self, L=1024, D=256, dropout=False, n_classes=1):
+        super(Attn_Net_Gated, self).__init__()
+        self.attention_a = [
+            nn.Linear(L, D),
+            nn.Tanh()]
 
+        self.attention_b = [nn.Linear(L, D),
+                            nn.Sigmoid()]
+        if dropout:
+            self.attention_a.append(nn.Dropout(0.25))
+            self.attention_b.append(nn.Dropout(0.25))
 
-        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        self.attention_a = nn.Sequential(*self.attention_a)
+        self.attention_b = nn.Sequential(*self.attention_b)
 
+        self.attention_c = nn.Linear(D, n_classes)
 
-        y = self.sigmoid(y)
-
-        return y
+    def forward(self, x):
+        a = self.attention_a(x)
+        b = self.attention_b(x)
+        A = a.mul(b)
+        A = self.attention_c(A)  # N x n_classes
+        return A, x
 
 class CAMIL(nn.Module):
-    def __init__(self, n_classes):
-        super(CAMIL, self).__init__()
-        self.pos_layer = MCAB(dim=512)
-        self._fc1 = nn.Sequential(nn.Linear(1024, 512), nn.ReLU())
-        self.cls_token = nn.Parameter(torch.randn(1, 1, 512))
+    def __init__(self, n_classes, input_dim=1024, temperature=3, dropout=0.25, n_layers=4, gate=True):
+        super().__init__()
+        # self.pos_layer = MCAB(dim=512,temperature=temperature)
+        self._fc1 = nn.Sequential(nn.Linear(input_dim, 512), nn.ReLU(), nn.Dropout(dropout))
+        # self.cls_token = nn.Parameter(torch.randn(1, 1, 512))
         self.n_classes = n_classes
-        self.layer1 = TransLayer(dim=512)
-        self.layer2 = TransLayer(dim=512)
+        # self.layer1 = TransLayer(dim=512)
+        # self.layer2 = TransLayer(dim=512)
+        self.layers = nn.ModuleList([
+            Block(dim=512,temperature=temperature) for _ in range(n_layers)
+        ])
         self.norm = nn.LayerNorm(512)
+
+        if gate:
+            attention_net = Attn_Net_Gated(L=512, D=256, dropout=False, n_classes=1)
+        else:
+            attention_net = Attn_Net(L=512, D=256, dropout=False, n_classes=1)
+
+        self.attention_net = attention_net
         self._fc2 = nn.Linear(512, self.n_classes)
+        initialize_weights(self)
 
     def forward(self, **kwargs):
-        h = kwargs['data'].float()
+        h = kwargs['data'].float()  # [B, n, 1024]
+        h = self._fc1(h)  # [B, n, 512]
 
-        h = self._fc1(h)
-        B = h.shape[0]
-        cls_tokens = self.cls_token.expand(B, -1, -1).cuda()
-        h = torch.cat((cls_tokens, h), dim=1)
-
-
-        h = self.layer1(h)
-
-        h = h + self.pos_layer(h)
-
-
-        h = self.layer2(h)
-
-
-        h = self.norm(h)[:, 0]
-
-
-        logits = self._fc2(h)
+        for layer in self.layers:
+            h = layer(h)
+        h = self.norm(h)
+        h = h.squeeze(0)
+        # ---->abmil pool
+        A, h = self.attention_net(h)
+        A = A.permute(1,0)
+        A = torch.softmax(A,dim=1)
+        h = torch.mm(A,h) 
+        # ---->predict
+        logits = self._fc2(h)  # [B, n_classes]
         Y_hat = torch.argmax(logits, dim=1)
         Y_prob = F.softmax(logits, dim=1)
         results_dict = {'logits': logits, 'Y_prob': Y_prob, 'Y_hat': Y_hat}
